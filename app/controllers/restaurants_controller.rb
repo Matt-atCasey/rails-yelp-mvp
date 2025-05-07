cdb890d19a87ae4743be70bb8aab18203b8f09f2
Matt Casey <matthew.casey@hotmail.co.uk>
2025-05-07 16:41:03 +0100

    done

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -9,9 +9,16 @@ class RestaurantsController < ApplicationController
   end
 
   def new
+    @restaurant = Restaurant.new
   end
 
   def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
   end
 
   def edit
@@ -22,4 +29,11 @@ class RestaurantsController < ApplicationController
 
   def destroy
   end
+
+private
+
+def restaurant_params
+  params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+end
+
 end
